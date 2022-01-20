47314f2b26d47eb0d63635278e5f84a3d290063d
LeoCofre <leocofre1981@gmail.com>
2022-01-19 23:08:22 -0300

    Retrofit

--- app/src/main/java/com/example/examplemvvm/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/examplemvvm/ui/view/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.example.examplemvvm.databinding.ActivityMainBinding
-import com.example.examplemvvm.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,5 +22,7 @@ class MainActivity : AppCompatActivity() {
             binding.tvAuthor.text = currentQuote.author
 
         })
+
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
     }
 }
--- app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
@@ -2,8 +2,6 @@ package com.example.examplemvvm.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.examplemvvm.model.QuoteModel
-import com.example.examplemvvm.model.QuoteProvider
 
 class QuoteViewModel : ViewModel() {
 
